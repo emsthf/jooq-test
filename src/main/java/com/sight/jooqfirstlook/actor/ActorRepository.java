ea04e071dfcccb68744d257740477f33db3a3ce1
emsthf <deunsol916@gmail.com>
2024-06-10 14:12:58 +0900

    feat: jOOQ로 insert, 벌크 쿼리 날리

--- src/main/java/com/sight/jooqfirstlook/actor/ActorRepository.java
+++ src/main/java/com/sight/jooqfirstlook/actor/ActorRepository.java
@@ -7,7 +7,7 @@ import org.jooq.generated.tables.JFilmActor;
 import org.jooq.generated.tables.daos.ActorDao;
 import org.jooq.generated.tables.pojos.Actor;
 import org.jooq.generated.tables.pojos.Film;
-import org.jooq.generated.tables.records.FilmRecord;
+import org.jooq.generated.tables.records.ActorRecord;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
 
@@ -85,5 +85,73 @@ public class ActorRepository {
             return DSL.noCondition();
         }
         return field.like("%" + inputValue + "%");
+    }
+
+    /**
+     * 이 부분이 지원되기까지 굉장히 많은 논의가 있었음
+     * jOOQ 3.19 부터 지원
+     * <p>
+     * 참고) https://github.com/jOOQ/jOOQ/issues/2536
+     *
+     * @return insert 시에 생성된 PK 값이 세팅된 pojo
+     */
+    public Actor saveByDao(Actor actor) {
+
+        // 이때 PK(actorId)가 actor 객체에 추가됨
+        actorDao.insert(actor);
+        return actor;
+    }
+
+    public ActorRecord saveByRecord(Actor actor) {
+        ActorRecord actorRecord = dslContext.newRecord(ACTOR, actor);
+        actorRecord.insert();
+        return actorRecord;
+    }
+
+    public Long saveReturningPkOnly(Actor actor) {
+        return dslContext.insertInto(
+                        ACTOR,
+                        ACTOR.FIRST_NAME,
+                        ACTOR.LAST_NAME,
+                        ACTOR.LAST_UPDATE
+                )
+                .values(
+                        actor.getFirstName(),
+                        actor.getLastName(),
+                        actor.getLastUpdate()
+                )
+                .returningResult(ACTOR.ACTOR_ID)
+                .fetchOneInto(Long.class);
+    }
+
+    public Actor saveReturning(Actor actor) {
+        return dslContext.insertInto(
+                        ACTOR,
+                        ACTOR.FIRST_NAME,
+                        ACTOR.LAST_NAME
+                )
+                .values(
+                        actor.getFirstName(),
+                        actor.getLastName()
+                )
+                .returningResult(ACTOR.fields())  // returning이 될 때 insert된 pk를 가지고 다시 조회를 함(총 2개의 쿼리가 나감)
+                .fetchOneInto(Actor.class);
+    }
+
+    public void bulkInsertWithRows(List<Actor> actorList) {
+        var rows = actorList.stream()
+                .map(actor -> DSL.row(
+                        actor.getFirstName(),
+                        actor.getLastName()
+                ))
+                .toList();
+
+        dslContext.insertInto(
+                        ACTOR,
+                        ACTOR.FIRST_NAME,
+                        ACTOR.LAST_NAME
+                )
+                .valuesOfRows(rows)
+                .execute();
     }
 }
---
+++ src/test/java/com/sight/jooqfirstlook/JooqInsertTest.java
@@ -0,0 +1,117 @@
+package com.sight.jooqfirstlook;
+
+import com.sight.jooqfirstlook.actor.ActorRepository;
+import org.jooq.generated.tables.pojos.Actor;
+import org.jooq.generated.tables.records.ActorRecord;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class JooqInsertTest {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @DisplayName("자동 생성된 DAO를 통한 insert")
+    @Transactional
+    @Test
+    void insert_dao() {
+        // given
+        Actor actor = new Actor();
+        actor.setFirstName("John");
+        actor.setLastName("Doe");
+        actor.setLastUpdate(LocalDateTime.now());
+
+        // when
+        actorRepository.saveByDao(actor);
+
+        // then
+        assertThat(actor.getActorId()).isNotNull();
+    }
+
+    @DisplayName("ActiveRecord를 통한 insert")
+    @Transactional
+    @Test
+    void insert_by_record() {
+        // given
+        Actor actor = new Actor();
+        actor.setFirstName("John");
+        actor.setLastName("Doe");
+        actor.setLastUpdate(LocalDateTime.now());
+
+        // when
+        ActorRecord actorRecord = actorRepository.saveByRecord(actor);
+
+        // then
+        assertThat(actorRecord.getActorId()).isNotNull();
+        assertThat(actor.getActorId()).isNull();
+    }
+
+    @DisplayName("SQL 실행 후 PK만 반환")
+    @Transactional
+    @Test
+    void insert_with_returning_pk() {
+        // given
+        Actor actor = new Actor();
+        actor.setFirstName("John");
+        actor.setLastName("Doe");
+        actor.setLastUpdate(LocalDateTime.now());
+
+        // when
+        Long pk = actorRepository.saveReturningPkOnly(actor);
+
+        // then
+        assertThat(pk).isNotNull();
+    }
+
+    @DisplayName("SQL 실행 후 해당 ROW 반환")
+    @Transactional
+    @Test
+    void insert_with_returning() {
+        // given
+        Actor actor = new Actor();
+        actor.setFirstName("John");
+        actor.setLastName("Doe");
+
+        // when
+        Actor newActor = actorRepository .saveReturning(actor);
+
+        // then
+        assertThat(newActor).hasNoNullFieldsOrProperties();
+    }
+
+    /**
+     * insert into `actor` (`first_name`, `last_name`, `last_update`) values (?, ?), (?, ?), (?, ?)
+     */
+    @DisplayName("bulk insert 예제")
+    @Transactional
+    @Test
+    void bulk_insert() {
+        // given
+        Actor actor1 = new Actor();
+        actor1.setFirstName("John");
+        actor1.setLastName("Doe");
+        actor1.setLastUpdate(LocalDateTime.now());
+
+        Actor actor2 = new Actor();
+        actor2.setFirstName("John2");
+        actor2.setLastName("Doe2");
+        actor2.setLastUpdate(LocalDateTime.now());
+
+        List<Actor> actorList = List.of(actor1, actor2);
+
+        // when
+        actorRepository.bulkInsertWithRows(actorList);
+
+        // then
+
+    }
+}
